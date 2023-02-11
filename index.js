84ebf2146f5dede9e0d241405736e616c5127d7a
GusBuzana <gubuzana@gmail.com>
2023-02-10 23:53:08 -0300

    Inserido display flex em todas versões, botão de limpar a tela e botões com links para redes

--- index.js
+++ index.js
@@ -1,105 +1,106 @@
-var keyLower = ["a", "e", "i", "o", "u"];
-var keyEncoded = ["ai", "enter", "imes", "ober", "ufat"];
+var keyLower = ["a", "e", "i", "o", "u"]
+var keyEncoded = ["ai", "enter", "imes", "ober", "ufat"]
 
-var inputMsg = "";
-var outputMsg = "";
-var input = document.querySelector("#msgIn");
-var output = document.querySelector("#msgOut");
-var noMsg = document.querySelector(".noMsg");
+var inputMsg = ""
+var outputMsg = ""
+var input = document.querySelector("#msgIn")
+var output = document.querySelector("#msgOut")
+var noMsg = document.querySelector(".noMsg")
 
 function encode() {
-    
-    outputMsg = "";
+  outputMsg = ""
 
-    inputMsg = input.value.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  inputMsg = input.value
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
 
-    for (i = 0; i < input.value.length; i++) {
-        
-        var letra = inputMsg[i];
+  for (i = 0; i < input.value.length; i++) {
+    var letra = inputMsg[i]
 
-        if (letra == keyLower[0]) {
-            outputMsg += keyEncoded[0];
-        }
-         else if (letra == keyLower[1]) {
-            outputMsg += keyEncoded[1];
-        }
-         else if (letra == keyLower[2]) {
-            outputMsg += keyEncoded[2];
-        }
-         else if (letra == keyLower[3]) {
-            outputMsg += keyEncoded[3];
-        }
-         else if (letra == keyLower[4]) {
-            outputMsg += keyEncoded[4];
-        }
-         else {
-            outputMsg += letra;
-
-        }
+    if (letra == keyLower[0]) {
+      outputMsg += keyEncoded[0]
+    } else if (letra == keyLower[1]) {
+      outputMsg += keyEncoded[1]
+    } else if (letra == keyLower[2]) {
+      outputMsg += keyEncoded[2]
+    } else if (letra == keyLower[3]) {
+      outputMsg += keyEncoded[3]
+    } else if (letra == keyLower[4]) {
+      outputMsg += keyEncoded[4]
+    } else {
+      outputMsg += letra
     }
-    output.value = outputMsg;
-    hide();
+  }
+  output.value = outputMsg
+  hide()
 }
 
 function decode() {
-    
-    outputMsg = "";
+  outputMsg = ""
 
-    outputMsg = input.value.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  outputMsg = input.value
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
 
-    for (i = 0; i <= 4; i++) {
-
-        if (outputMsg.includes("ai")) {
-            outputMsg = outputMsg.replace(/ai/g, "a");
-        }
-         else if (outputMsg.includes("enter")) {
-            outputMsg = outputMsg.replace(/enter/g, "e");
-        }
-         else if (outputMsg.includes("imes")) {
-            outputMsg = outputMsg.replace(/imes/g, "i");
-        }
-         else if (outputMsg.includes("ober")) {
-            outputMsg = outputMsg.replace(/ober/g, "o");
-        }
-         else if (outputMsg.includes("ufat")) {
-            outputMsg = outputMsg.replace(/ufat/g, "u");
-        }
+  for (i = 0; i <= 4; i++) {
+    if (outputMsg.includes("ai")) {
+      outputMsg = outputMsg.replace(/ai/g, "a")
+    } else if (outputMsg.includes("enter")) {
+      outputMsg = outputMsg.replace(/enter/g, "e")
+    } else if (outputMsg.includes("imes")) {
+      outputMsg = outputMsg.replace(/imes/g, "i")
+    } else if (outputMsg.includes("ober")) {
+      outputMsg = outputMsg.replace(/ober/g, "o")
+    } else if (outputMsg.includes("ufat")) {
+      outputMsg = outputMsg.replace(/ufat/g, "u")
     }
-    output.value = outputMsg;
-    hide();
+  }
+  output.value = outputMsg
+  hide()
 }
 
-
 function copy() {
+  output.select()
+  output.setSelectionRange(0, 99999)
 
-    output.select();
-    output.setSelectionRange(0, 99999); 
+  navigator.clipboard.writeText(output.value)
 
-    navigator.clipboard.writeText(output.value);
-  
-    var copyTxt = document.querySelector(".copyTxt");
-    copyTxt.innerHTML = "Copiado";
-  }
-
+  var copyTxt = document.querySelector(".copyTxt")
+  copyTxt.innerHTML = "Copiado"
+}
 
 function copyOut() {
-
-    var copyTxt = document.querySelector(".copyTxt");
-    copyTxt.innerHTML = "Copiar para área de transferência";
+  var copyTxt = document.querySelector(".copyTxt")
+  copyTxt.innerHTML = "Copiar para área de transferência"
 }
 
 function hide() {
-    noMsg.style.display = "none";
-    output.style.display = "block";
+  noMsg.style.display = "none"
+  output.style.display = "block"
 }
 
+function show() {
+  noMsg.style.display = "block"
+  output.style.display = "none"
+}
+
+function clear() {
+  var cleaned = ""
+  output.value = cleaned
+  input.value = cleaned
+  input.focus()
+  show()
+}
 
 var bttCop = document.querySelector(".copy")
-var bttEnc = document.querySelector(".encode");
-var bttDec = document.querySelector(".decode");
+var bttEnc = document.querySelector(".encode")
+var bttDec = document.querySelector(".decode")
+var bttClear = document.querySelector(".clear")
 
-bttCop.onclick = copy;
-bttCop.onmouseout = copyOut;
-bttEnc.onclick = encode;
-bttDec.onclick = decode;
-
+bttCop.onclick = copy
+bttCop.onmouseout = copyOut
+bttEnc.onclick = encode
+bttDec.onclick = decode
+bttClean.onclick = clear
